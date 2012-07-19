bb73dd40bc03ac28d120c363edc875a347584910
admin <admin@192.168.1.100>
2012-07-19 13:05:19 +0300

    added empty line to class1

--- src/testClass1.java
+++ src/testClass1.java
@@ -2,5 +2,6 @@ public class testClass1 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("this is masterBranch");
+		System.out.println();
 	}
 }
